20582b3e6f3e0c4fa3af248aa5c3615c4aeca014
Scott Malkie <s@linux.com>
2016-05-21 14:17:51 -0400

    Chapter 4: power32.s
    Change to Hexadecimal, confirm function

--- power32.s
+++ power32.s
@@ -1,5 +1,3 @@
-#### assemble with as --32 and link with ld -melf_i86 ####
-
 # PURPOSE: Program to illustrate how functions work
 #          This program will compute the value of 2^3 + 5^2
 #
@@ -12,34 +10,34 @@
 .globl _start
 
 _start:
-  pushl $0x3	    # push second argument
-  pushl $0x2            # push first argument
-  call power	    # call the function
-  addl $0x8, %esp	    # move the stack pointer back
-  
-  pushl %eax	    # save the first answer before
-                        # calling the next function
+  pushl $0x3	# push second argument
+  pushl $0x2        # push first argument
+  call power	# call the function
+  addl $0x8, %esp   # move the stack pointer back 2 longs  
 
-  pushl $0x2            # push second argument
-  pushl $0x5            # push first argument
-  call power            # call the function
-  addl $0x8, %esp       # move the stack pointer back
+  pushl %eax	# save the first answer onto the stack before
+                    # calling the next function
+
+  pushl $0x2        # push second argument
+  pushl $0x5        # push first argument
+  call power        # call the function
+  addl $0x8, %esp   # move the stack pointer back 2 longs
   
-  popl %ebx             # the second answer is already in %eax
-                        # we saved the first answer onto the stack
-                        # so now we just pop it out to %ebx
+  popl %ebx         # the second answer is already in %eax
+                    # we saved the first answer onto the stack
+                    # so now we just pop it out to %ebx
                     
-  addl %eax, %ebx       # add the two answers together
-                        # answer is stored in destination
+  addl %eax, %ebx   # add the two answers together
+                    # answer is stored in destination
   
-  movl $0x1, %eax       # syscall for exit
-  int $0x80             # call the kernel
+  movl $0x1, %eax   # syscall for exit
+  int $0x80         # call the kernel
   
 # PURPOSE: This function is used to compute the value
 #         of a number raised to a power.
 #
 # INPUT: First argument - the base number
-#       Second argument - the power to raise it to
+#        Second argument - the power to raise it to
 #
 # OUTPUT: Will give the result as a return value
 #
@@ -48,35 +46,33 @@ _start:
 # VARIBLES:
 #          %ebx holds the base number
 #          %ecx holds the exponent
-#          -4(%ebp) holds the current result
+#          -0x4(%ebp) holds the current result
 #          %eax is used for temporary storage
 #
 
 .type power, @function
 power:
-  pushl %ebp            # save old base pointer
-  movl %esp, %ebp       # make stack pointer the base pointer
-  subl $0x4, %esp       # get room for our local storage
+  pushl %ebp              # save old base pointer
+  movl %esp, %ebp         # make stack pointer the base pointer
   
-  movl 4(%ebp), %ebx    # put first argument in %eax
-  movl 8(%ebp), %ecx    # put second argument in %ecx
-  movl %ebx, -4(%ebp)   # store current result
+  subl $0x4, %esp         # get room for our local storage
+  
+  movl 0x8(%ebp), %ebx    # put first argument in %ebx
+  movl 0xC(%ebp), %ecx    # put second argument in %ecx
+  movl %ebx, -0x4(%ebp)   # store current result
   
 power_loop_start:
-  cmpl $0x1, %ecx       # compare exponent to 1, we are done if so
-  je end_power          # end and return initial value
-  movl -4(%ebp), %eax   # move the current result into %eax
-  imull %ebx, %eax      # multiply the current result by the base number
-  movl %eax, -4(%ebp)   # store the current result
-  decl %ecx             # decrease the power
-  jmp power_loop_start  # run for the next power
+  cmpl $0x1, %ecx         # compare exponent to 1, we are done if so
+  je end_power            # end and return initial value
+  movl -0x4(%ebp), %eax   # move the current result into %eax
+  imull %ebx, %eax        # multiply the current result by the base number
+  movl %eax, -0x4(%ebp)   # store the current result
+  decl %ecx               # decrease the power
+  jmp power_loop_start    # run for the next power
  
 end_power:
-  movl -4(%ebp), %eax   # return value goes in %eax
-  movl %ebp, %esp       # restore the stack pointer
-  popl %ebp             # restore the base pointer
+  movl -0x4(%ebp), %eax   # return value goes in %eax
+  movl %ebp, %esp         # restore the stack pointer
+  popl %ebp               # restore the base pointer
   ret
 
-     
-  
-              
